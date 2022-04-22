50992824f7949bfb787a6f23ed4149b56babeeeb
PdskDev <prestadesk@gmail.com>
2022-04-22 20:33:40 +0200

    API Get, POST Added

--- Controllers/CakeController.cs
+++ Controllers/CakeController.cs
@@ -21,5 +21,21 @@ public class CakeController: ControllerBase
         var cakes = await _currentDbContext.Cake.ToListAsync();
         return Ok(cakes);
     }
+
+    [HttpGet]
+    [Route("get-cake-by-id")]
+    public async Task<IActionResult> GetCakeByIdAsync(int id)
+    {
+        var cake = await _currentDbContext.Cake.FindAsync(id);
+        return Ok(cake);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Cake cake)
+    {
+        _currentDbContext.Cake.Add(cake);
+        await _currentDbContext.SaveChangesAsync();
+        return Created($"/get-cake-by-id?id={cake.Id}", cake);
+    }
         
 }
